b96566d93f2339ca51f3c062083ef80c1d5baeed
SnapdCC <nilesb9960@my.uwstout.edu>
2023-05-23 15:50:03 -0500

    Minor level layout changes

--- Assets/Scripts/Hazards/Refactored/Capstan.cs
+++ Assets/Scripts/Hazards/Refactored/Capstan.cs
@@ -4,21 +4,14 @@ using UnityEngine;
 
 public class Capstan : Hazard
 {
-    [SerializeField] GameObject CapstanArms=null;
     [SerializeField] float rotationSpeed = 10;
-    private Rigidbody rb;
 
     void Start()
     {
-        if (CapstanArms != null)
-        {
-            rb = CapstanArms.GetComponent<Rigidbody>();
-        }
     }
 
     void FixedUpdate()
     {
-        rb.angularVelocity = new Vector3(0, rotationSpeed, 0);
-        kbMod = rotationSpeed;
+        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
     }
 }

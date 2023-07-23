cb5ff71092471cd0afbfa2a34ea680fbb9d9bea9
SnapdCC <nilesb9960@my.uwstout.edu>
2023-07-23 18:16:25 -0500

    Made the time the boss waits before he charges modular and made it faster by default

--- Assets/Scripts/BossRelated/TestBossAI.cs
+++ Assets/Scripts/BossRelated/TestBossAI.cs
@@ -13,6 +13,7 @@ public class TestBossAI : CharacterMovement
     Vector3 chargeDirection;
     Quaternion chargeAngle;
     [SerializeField] GameObject StunVFX;
+    [SerializeField] float PrepTime = 3.0f;
 
 
     bool canCharge = true;
@@ -110,7 +111,7 @@ public class TestBossAI : CharacterMovement
         }
         
         chargeTarget.transform.position = targetedPlayer.transform.position;
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(PrepTime);
         prepping = false;
         chargeDirection = (chargeTarget.transform.position - this.transform.position).normalized;
         chargeTarget.transform.position += chargeDirection * 100;
